59a95c6d9d40e17114465b5952e583146b38f62f
gustaff <gustaffson@users.noreply.github.com>
2018-06-21 02:45:18 +0100

    Rework completo.

--- ws-axess.py
+++ ws-axess.py
@@ -1,130 +1,73 @@
-'''
-  Ax Webservice - ver. 0.4
+"""
+  Ax Webservice - ver. 0.80
   Developed by Gustavo Pinto - Módulos Flask, Flask Restful, sqlite3 e datetime
   !/usr/bin/python
   -*- coding: utf8 -*-
-'''
+"""
 
 import sqlite3
-import re
 from flask import Flask, request
 from flask_restful import Api
-from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
 # Parameters
 som, rele, sound_deny, sound_accept, porta = "beep=1", "relay=1,50", "beep=2", "beep=1", "5002"
-conn = sqlite3.connect('axess-ws.db')
-card = '?'
-term_id = '?'
+card = ''
+term_id = ''
 resultado = 0
-
-def check_mov():
-    global id_transaction
-    global direction
-    global term_id
-    global card
-    card, direction, term_id = process_request()[0], process_request()[1], process_request()[2]
-    select_id = conn.execute("select max(idTransaction) from transactions")
-    id_transaction = select_id.fetchone()
-
-def store_mov():
-    global id_transaction
-    global direction
-    global term_id
-    global card
-    card, direction, term_id = process_request()[0], process_request()[1], process_request()[2]
-    select_id = conn.execute("select max(idTransaction) from transactions")
-    id_transaction = select_id.fetchone()
-    if id_transaction[0] is None:
-        conn.execute("insert into transactions values (?, ?, ?, ?, ?)",
-                     (1, "0000000000000000", datetime.now(), "INIT BD", "99"))
-        conn.commit()
-        select_id = conn.execute("select max(idTransaction) from transactions")
-        id_transaction = select_id.fetchone()
-        conn.execute("insert into transactions values (?, ?, ?, ?, ?)",
-                     ((int(id_transaction[0]) + 1), card, datetime.now(), term_id, direction))
-        conn.commit()
-        conn.close()
-
-def process_request():
-    global id_transaction
-    global direction
-    global term_id
-    global card
-    req_uri = request.url
-    start_transaction = (req_uri.find("?"))
-    end_transaction = (req_uri.find("&"))
-    end_term_id = (req_uri.find("&", (end_transaction + 1)))
-    raw_transaction = req_uri[start_transaction:end_transaction]
-    term_id = req_uri[(end_transaction + 4):end_term_id]
-    raw_right_date = raw_transaction.find(",")
-    raw_right_time = raw_transaction.find(",", raw_right_date + 1)
-    raw_right_direction = raw_transaction.find(",", raw_right_time + 1)
-    raw_left_card = raw_transaction.find(",", raw_right_direction + 1)
-    direction = raw_transaction[(raw_right_time + 1):(raw_right_time + 2)]
-    raw_right_card = raw_transaction.find(",", raw_left_card + 1)
-    final = (raw_transaction[(raw_left_card + 1):raw_right_card], direction, term_id)
-    return final
 
 
 def resultado_terminal():
-    if (resultado == 0):
-        return sound_deny + "\r\n"
-    elif (resultado == 1):
+    if resultado == 1:
         return sound_accept + "\r\n" + rele + "\r\n"
+    else:
+        return sound_deny + "\r\n"
 
 
-val_entrada = conn.execute("select count(idCard) from cards where apbStatus = 0 and cardUid = " + '"' + card + '"')
-val_saida = conn.execute("select count(idCard) from cards where apbStatus = 1 and cardUid = " + '"' + card + '"')
-val_livre = conn.execute("select count(idCard) from cards where freeAcess = 1")
-
-if val_entrada == 0 and direction == 1 and val_livre is not 1:  # VALIDAÇÃO DE ENTRADA SEM ACESSO LIVRE
-    conn.execute("update cards set apbStatus = 1 where cardUid = " + '"' + card + '"')
-    select_id = conn.execute("select max(idTransaction) from transactions")
-    id_transaction = select_id.fetchone()
-    conn.execute("insert into transactions values (?, ?, ?, ?, ?)",
-                 ((int(id_transaction[0]) + 1), card, datetime.now(), term_id, direction))
-    conn.commit()
-    resultado = 1
-    resultado_terminal()
-elif val_saida == 1:  # VALIDAÇAO DE SAÍDA
-    conn.execute('update cards set apbStatus = 0 where cardUid = ' + '"' + card + '"')
-    select_id = conn.execute("select max(idTransaction) from transactions")
-    id_transaction = select_id.fetchone()
-    conn.execute("insert into transactions values (?, ?, ?, ?, ?)",
-                 ((int(id_transaction[0]) + 1), card, datetime.now(), term_id, direction))
-    conn.commit()
-    resultado = 1
-    resultado_terminal()
-else:  # ACESSO RECUSADO COM LOG
-    select_id = conn.execute("select max(idTransaction) from transactions")
-    id_transaction = select_id.fetchone()
-    conn.execute('insert into transactions values (?, ?, ?, ?, ?)',
-                 ((int(id_transaction[0]) + 1), card, datetime.now(), term_id, 3))
-    conn.commit()
-    resultado = 0
-    resultado_terminal()
-
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
     global id_transaction
     global direction
     global term_id
     global card
+    global transaction_stamp
+    global date_transaction
+    global time_transaction
+    global resultado
     pedido = path
-    regexp = re.compile(r'ba[rzd]')
-    if regexp.search(online):
-        process_request()
-        ticket = check_mov()
-        return ticket
-    if pedido == "batch":
-        process_request()
-        store_mov()
+    if 'online' in pedido:
+        mensagem=request.args
+        print()
+        print("NOME DO TERMINAL: ", mensagem['id'])
+        print("MAC ADDRESS: ", mensagem['mac'])
+        transaction_stamp = mensagem['trsn']
+        transaction_split = transaction_stamp.split(",")
+        card = transaction_split[4]
+        print()
+        print("CARTÃO UID:\r\n", card)
+        date_transaction = transaction_split[0]
+        print("DATA DO MOVIMENTO:\r\n", date_transaction[-2:] + "-" + date_transaction[4:6] + "-" + date_transaction[:4])
+        time_transaction = transaction_split[1]
+        print("HORA DO MOVIMENTO:\r\n", time_transaction[:2] + ":" + time_transaction[2:4] + ":" + time_transaction[-2:])
+        print()
+        conn = sqlite3.connect('axess-ws.db')
+        cursor = conn.execute('SELECT count(*) FROM cards WHERE cardUid = "' + str(card) + '"')
+        rows = cursor.fetchone()
+        contador = int(rows[0])
+        print("VÁLIDOS EM BD: ", contador)
+        print()
+        if contador == 1:
+            resultado = 1 # ACEITAR MOVIMENTO
+            return sound_accept + "\r\n" + rele + "\r\n"
+        else:
+            resultado = 0 # REJEITAR MOVIMENTO!
+            return sound_deny + "\r\n"
+    elif 'batch' in pedido:
         return "ack=1" + "\r\n"
-    elif pedido == "keepalive":
+    elif 'keepalive' in pedido:
         return "ack=1" + "\r\n"
     else:
         texto_final = "Tipo de pedido não suportado! Verifique a documentação!" + "\r\n" + "Grupo Copigés - suporte@copiges.pt" + "\r\n"
